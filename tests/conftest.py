022be7a611815920d93186461eb78abdac3362b8
Victor Pereira <victtor.vpb@gmail.com>
2021-08-21 23:28:52 -0300

    fix(conftest): fix pep8 import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import sys
 from typing import Generator
 
+import pytest  # noqa:E402
+
 sys.path = ["", ".."] + sys.path[1:]  # noqa:E402
 
-import pytest
-from fastapi.testclient import TestClient
+
+from fastapi.testclient import TestClient  # noqa:E402
 from main import create_app  # noqa:E402
 
 app = create_app()
